a7c34e8697bb967adb2c6ebdca02e65c0f4dec80
“bdbdbdsbd“ <1378984560@qq.com>
2021-08-01 23:18:28 +0800

    4.1.0

--- 4/JS/main.js
+++ 4/JS/main.js
@@ -24,6 +24,7 @@ const people = [
 //1.find the prople who born between 1500 and 1600
 //   filter()创建新的类型化数组
 // filter方法对类型化数组中的元素调用提供的 callback函数，并且会为callback返回 true 的那些元素构造新的类型化数组。
+// => 这个符号可以理解为一个函数
 console.log("选出在1500-1600之间出生的人");
 const fifteen = inventors.filter(function (inventor) {
     if(inventor.year >= 1500 && inventor.year <1600){
@@ -36,6 +37,36 @@ const fifteen2 = inventors.filter( inventor => (inventor.year >= 1500 && invento
 console.log(fifteen2);
 
 //2.输出所有人的姓名
+// 一个Map对象在迭代时会根据对象中元素的插入顺序来进行 — 一个  for...of 循环在每次迭代后会返回一个形式为[key，value]的数组。
 console.log("输出所有人的姓名");
 const fullname=inventors.map(inventor => `${inventor.first}, ${inventor.last}`);
 console.log(fullname);
+
+//3.按照生日从长到幼排序
+// compareFunction 可选用来指定按某种顺序进行排列的函数。如果省略，元素按照转换为的字符串的各个字符的Unicode位点进行排序。
+inventors.sort(function(a,b){
+    if(a.year>b.year){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+})
+console.log("按照生日从长到幼排序");
+console.log(inventors);
+inventors.sort((a,b) => a.year<b.year?1:-1);
+console.log("按照生日从幼到长排序");
+console.log(inventors);
+
+//4.这些人活了多久
+console.log("这些人活了多久");
+var totalYears1 = 0;
+for (let index = 0; index < inventors.length; index++) {
+     totalYears1 += inventors[index].passed- inventors[index].year;   
+}
+console.log(totalYears1);
+var totalYears2 = 0;
+// reduce() 方法对数组中的每个元素执行一个由您提供的reducer函数(升序执行)，将其结果汇总为单个返回值。
+// arr.reduce(callback(accumulator, currentValue[, index[, array]])[, initialValue])
+totalYears2 = inventors.reduce((total,inventor) => {return total+(inventor.passed-inventor.year);},0);
+console.log(totalYears2);
