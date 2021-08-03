45445731c78b0b788fbc47b7c8b68d000a4af592
“bdbdbdsbd“ <1378984560@qq.com>
2021-08-03 14:07:07 +0800

    6.0.3

--- 6/JS/main.js
+++ 6/JS/main.js
@@ -1,5 +1,6 @@
 window.onload=function(){
     const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+    // object
     const cities=[];
     // fetch
     // Fetch API 提供了一个 JavaScript 接口，用于访问和操纵 HTTP 管道的一些具体部分，例如请求和响应。它还提供了一个全局 fetch() 方法，该方法提供了一种简单，合理的方式来跨网络异步获取资源。
@@ -17,6 +18,6 @@ window.onload=function(){
     prom.then(Blob =>(Blob.json())).then(data =>cities.push(...data));
     // .then(data => cities=(data));
     // 不加...这1000个数据就会被当作一个元素添加进去
-    console.log(cities[1]);
+    console.log((cities));
 }
 
