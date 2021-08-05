97b2081a4d02bdcb36b45bfe2654214eec852e75
“bdbdbdsbd“ <1378984560@qq.com>
2021-08-05 17:06:58 +0800

    11.3.0

--- 11/JS/main.js
+++ 11/JS/main.js
@@ -1,9 +1,10 @@
 var line =document.querySelector(".line");
+var lline =document.querySelector(".lline");
 var video = document.querySelector("video");
 var playbutton = document.querySelector(".toggle");
 var backbutton = document.querySelectorAll("[data-skip]");
 console.log(backbutton);
-
+var inputr = document.querySelectorAll("input");
 function togglePlay(){
     const method = video.paused ? 'play':"pause";
     video[method]();
@@ -13,14 +14,28 @@ function togglePlay(){
 
 function skipp(){
     // parseFloat() 函数可解析一个字符串，并返回一个浮点数。
-    console.log(this.dataset.skip);
+    // console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
 function updateline(){
     line.style.width = `${video.currentTime/video.duration*100}%`;
+}
+function handleRangeUpdate(){
+    // console.log("this.name");
+    // console.log("this.value");
+    video[this.name]=this.value;
+}
+function updateVideo(e){
+    console.log(e.offsetX);
+    // console.log(this);
+    const upTime = e.offsetX/video.offsetWidth*video.duration;
+    console.log(upTime);
+    video.currentTime = parseFloat(`${upTime}%`);
 }
 video.addEventListener("click",togglePlay);
 video.addEventListener("timeupdate",updateline);
 // video.addEventListener("paused",updateButton);
 playbutton.addEventListener("click",togglePlay);
 backbutton.forEach(bb => bb.addEventListener("click",skipp));
+inputr.forEach(inr =>inr.addEventListener("mousemove",handleRangeUpdate));
+lline.addEventListener("click",updateVideo);
