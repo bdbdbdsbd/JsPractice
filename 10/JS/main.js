37fd0a78f8b53a9d7e78e16bf8864995e7b1cc25
“bdbdbdsbd“ <1378984560@qq.com>
2021-08-05 13:50:04 +0800

    10.1.0

--- 10/JS/main.js
+++ 10/JS/main.js
@@ -1,7 +1,27 @@
+var flag_click=false;
+var last_click;
+var check;
 window.onload=function(){
-    const check = document.querySelectorAll("input");
+    var check = document.querySelectorAll("input");
     check.forEach(ch => ch.addEventListener("click",change));
-}
-function change(e){
-    e.srcElement.parentNode.classList.toggle('cl');
+    function change(e){  
+        if(e.shiftKey && this.checked){
+            check.forEach(che =>{
+                if(che==last_click || che==this){
+                    flag_click=!flag_click; 
+                }
+                if(flag_click){
+                    che.parentNode.classList.add('cl');
+                    che.checked =true;
+                }
+                else{
+                    che.parentNode.classList.remove('cl');
+                }
+                if(che==last_click || che==this){
+                    che.parentNode.classList.add('cl');
+                }
+            });
+        }
+        last_click = this;
+    }
 }
