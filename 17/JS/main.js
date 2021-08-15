b392c77424d106927159abc963e9d7b5356008d5
“bdbdbdsbd“ <1378984560@qq.com>
2021-08-15 20:22:50 +0800

    17.0.0

--- 17/JS/main.js
+++ 17/JS/main.js
@@ -2,5 +2,15 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 // 必须是1和-1
 const orderBands = bands.sort((a,b) => {return a>b?1:-1;})
 console.log(orderBands);
+//  trim() 方法用于删除字符串的头尾空格。
 orderBands.forEach(i => { console.log(i.replace(/^(a|The|An)/i," ").trim()); });
 console.log(orderBands);
+
+window.onload= function(){
+    document.querySelector(".main").innerHTML =
+    orderBands.map(item => {
+        return `<li>${item}</li>`;
+    }
+    
+    ).join('');
+}
