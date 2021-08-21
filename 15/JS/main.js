ca1b0671aaf3f5d18302b1cd76eafe8b76985812
bdbdbdsdb <1378984560@qq.com>
2021-08-21 17:25:47 +0800

    15.0.0

--- 15/JS/main.js
+++ 15/JS/main.js
@@ -1,40 +1,46 @@
 // window.onload = function(){
-    // 应该加form 而不是那个按键
-    // items是存储的值
-    // itemList是根据items来改变的元素
-    const submit_item = document.querySelector("form");
-    var itemList = document.querySelector(".itemList");
-    textItem = document.querySelector('[name=item]');
-    // items =[];
-    var items =JSON.parse(localStorage.getItem("items"));
-    populateList(items,itemList);
-    var smallItems = document.querySelectorAll(".smallItem");
-    function addItems(e){
-        // 不刷新页面了
-        e.preventDefault();
-        const text = textItem.value;
-        const item ={
-            text:text,
-            done:false
-        }
-        items.push(item);
-        // 开始了，存进local 存储里面
-        // 但是不能直接放，因为是转换成数组的
-        // localStorage.setItem("items",items);
-        localStorage.setItem("items",JSON.stringify(items));
-        this.reset();
-        e.preventDefault();
-        console.log(items);
-        populateList(items,itemList);
-        smallItems.forEach(it => it.addEventListener('change',toggleDone));
+// 应该加form 而不是那个按键
+// items是存储的值
+// itemList是根据items来改变的元素
+const submit_item = document.querySelector("form");
+var itemList = document.querySelector(".itemList");
+textItem = document.querySelector('[name=item]');
+var items = JSON.parse(localStorage.getItem("items"));
+if (items == null) {
+    items = [];
+}
+populateList(items, itemList);
+var smallItems = document.querySelectorAll(".smallItem");
+
+function addItems(e) {
+    // 不刷新页面了
+    e.preventDefault();
+    const text = textItem.value;
+    const item = {
+        text: text,
+        done: false
     }
-    
-    function populateList( plates ,platesList){
-        // console.log(platesList)
-        platesList.innerHTML=plates.map( (plate,i) => {
-            let checkFlag = plate.done?"checked":"";
-            console.log(checkFlag);
-            return `
+    items.push(item);
+    // 开始了，存进local 存储里面
+    // 但是不能直接放，因为是转换成数组的
+    // localStorage.setItem("items",items);
+    localStorage.setItem("items", JSON.stringify(items));
+    this.reset();
+    e.preventDefault();
+    console.log(items);
+    if (itemList != null) {
+        populateList(items, itemList);
+        console.log(items);
+    }
+    smallItems.forEach(it => it.addEventListener('change', toggleDone));
+}
+
+function populateList(plates, platesList) {
+    // console.log(platesList)
+    platesList.innerHTML = plates.map((plate, i) => {
+        let checkFlag = plate.done ? "checked" : "";
+        console.log(checkFlag);
+        return `
             <div class="smallItem">
             
             <li>
@@ -43,22 +49,19 @@
             </li>
             </div>
             `;
-        }
-        ).join('');
-    }
+    }).join('');
+}
 
-    function toggleDone(e){
-        console.log(e.target);
-        // 读取数据来了
-        index = (e.target.dataset.index);
-        // console.log(itemList[index].innerHTML);
-        items[index].done = !items[index].done
-        console.log(items[index].done);
-        // console.log(this);
-        localStorage.setItem("items",JSON.stringify(items));
-    }
-    // console.log(itemList);
-    submit_item.addEventListener('submit',addItems);
-    smallItems.forEach(it => it.addEventListener('change',toggleDone));
-
-    
+function toggleDone(e) {
+    console.log(e.target);
+    // 读取数据来了
+    index = (e.target.dataset.index);
+    // console.log(itemList[index].innerHTML);
+    items[index].done = !items[index].done
+    console.log(items[index].done);
+    // console.log(this);
+    localStorage.setItem("items", JSON.stringify(items));
+}
+// console.log(itemList);
+submit_item.addEventListener('submit', addItems);
+smallItems.forEach(it => it.addEventListener('change', toggleDone));
